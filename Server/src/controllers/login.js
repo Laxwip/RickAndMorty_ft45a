94bcf8b4503707cb03efd00419e4b5db01bdb1b2
Laxwip <pachecogam135583@gmail.com>
2023-12-19 08:44:52 -0500

    m4-2

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,16 +1,35 @@
-const users = require("../utils/users")
+const { User } = require("../DB_connection")
 
-const login = (req, res) =>{
-  const email = req.query.email;
-  const password = req.query.password
-
-  const matchingUsers = users.find(user => email === user.email && password === user.password)
-
-  if(matchingUsers){
-    res.status(200).json({access: true})
-  } else {
-    res.status(200).json({access: false})
+const login = async (req, res) =>{
+  try {
+    const email = req.query.email;
+    const password = req.query.password
+    if(email && password){
+      const actualUser = await User.findOne({
+        where: {email: email}
+      })
+      if(actualUser){
+        if(actualUser.password === password){
+          return res.status(200).json({
+            access:true
+          });
+        }
+        return res.status(403).send("Contraseña incorrecta")
+      }
+      return res.status(404).send("Usuario no encontrado")
+    }
+    return res.status(400).send("Faltan datos")
+  } catch (error) {
+    return res.status(500).send(error.message)
   }
+
 }
 
 module.exports = login
+// const matchingUsers = User.find(user => email === user.email && password === user.password)
+
+// if(matchingUsers){
+//   res.status(200).json({access: true})
+// } else {
+//   res.status(400).send("Faltan datos")
+// }
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -6,14 +6,18 @@ const router = express.Router()
 //+ CONTROLADORES ( importados )
 //° Son los que se comunican directamente con la base de datos 
 const getCharById = require("../controllers/getCharById")
-const {allFav, postFav, deleteFav} = require("../controllers/handleFavorites")
-const login = require("../controllers/login")
+
+const login = require("../controllers/login");
+const postUser = require("../controllers/postUser");
+const postFav = require("../controllers/postFav");
+const deleteFav = require("../controllers/deleteFav");
 
 //+ RUTAS 
 //° Redireccionan de acuerdo a la ruta y ejecutan una funcion sin interactuar directamente con la base de datos
 router.get("/character/:id", getCharById)
+
 router.get("/login", login)
-router.get("/fav", allFav)
+router.post("/login", postUser)
 router.post("/fav", postFav)
 router.delete("/fav/:id", deleteFav)
 
